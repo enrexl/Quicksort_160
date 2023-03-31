cab9ace966aa1124c466268e856ecbc9e8db310b
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-03-31 10:59:53 +0700

    membuat fungsi pertukaran (swap)

--- Quicksort_160/Quicksort_160.cpp
+++ Quicksort_160/Quicksort_160.cpp
@@ -27,6 +27,14 @@ void input() {
         cin >> arr[i];
     }
 }
+//swap the element at index x with the element at index y
+void swap(int x, int y) {
+    int temp;
+    temp = arr[x];
+    arr[x] = arr[y];
+    arr[y] = temp;
+}
+
 
 int main()
 {
