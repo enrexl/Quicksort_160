3f6d22fe0afda1648a0283d809afcadfd2c3f656
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-03-31 10:54:06 +0700

    Membuat perulangan while dan if else didalam fungis input

--- Quicksort_160/Quicksort_160.cpp
+++ Quicksort_160/Quicksort_160.cpp
@@ -7,7 +7,18 @@ int cmp_count = 0; //counter perbandingan
 int mov_count = 0; //counter pertukaran/swap
 int n;
 
+void input() {
+    while (true) {
+        cout << "Masukkan panjang element array :";
+        cin >> n;
 
+        if (n <= 20)
+            break;
+        else
+            cout << "\n Maksimum panjang array adalah 20" << endl;
+    }
+
+}
 
 int main()
 {
