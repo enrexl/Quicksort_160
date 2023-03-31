732274cb370f74b577b8ed2b9d38d703d4bc412c
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-03-31 10:51:41 +0700

    Membuat variable int

--- Quicksort_160/Quicksort_160.cpp
+++ Quicksort_160/Quicksort_160.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+//array of integers untuk menyimpan value
+int arr[20];
+int cmp_count = 0; //counter perbandingan
+int mov_count = 0; //counter pertukaran/swap
+int n;
+
+
+
 int main()
 {
     std::cout << "Hello World!\n";
